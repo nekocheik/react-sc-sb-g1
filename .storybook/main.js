3ce51987daee8c2de7dce71469b159a2f8661364
cheik <cheik.kone@hetic.net>
2020-05-23 21:46:06 +0200

    add accécibility

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,3 @@
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin')
-
 module.exports = {
   stories: ['../src/**/*.stories.(js|mdx)'],
   addons: [
@@ -12,7 +10,7 @@ module.exports = {
       },
     },
     '@storybook/addon-docs/register',
-    '@storybook/addon-a11y/register'
+    '@storybook/addon-a11y/register',
   ],
 
   
